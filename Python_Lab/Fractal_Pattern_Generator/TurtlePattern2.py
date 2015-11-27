35817a035a7b4265db9dead8cbcf02e3195bd901
carlinaylsworth <carlinaylsworth@hotmail.com>
2015-11-26 23:48:53 -0800

    Added simple fractal pattern generator to Python Code Lab

---
+++ Python_Lab/Fractal_Pattern_Generator/TurtlePattern2.py
@@ -0,0 +1,67 @@
+import turtle
+
+
+def draw_shape(a_turtle, size, sides):
+    
+    a_turtle.begin_fill()
+    a_turtle.down()
+    
+    for i in range(sides):
+        a_turtle.forward(size)
+        a_turtle.right(360/sides)
+        
+    a_turtle.up()
+    a_turtle.end_fill()
+
+    
+def draw_fractal():
+    
+    window = turtle.Screen()
+    window.bgcolor("tomato")
+
+    #Play with these variables to get different patterns!
+    starting_coordinates = (150,-150)
+    starting_angle= 180
+    sides = 3
+    length = 20
+    turns = 3
+    spacing_multiplier = 2
+    shape_shrink_factor = 1
+
+    #create turtle and set it's properties
+    ash = turtle.Turtle()
+    ash.shape("turtle")
+    ash.color("teal")
+    ash.speed(100)
+    ash.penup()
+    ash.goto(starting_coordinates)
+    ash.pendown()
+
+    #And the turtle starts drawing!
+    ash.right(starting_angle)
+    for i in range(sides):
+        for i in range(sides):
+            for i in range(sides):
+                for i in range(sides):
+                    draw_shape(ash, length/shape_shrink_factor, sides)
+                    ash.forward(length*spacing_multiplier)
+                    ash.right(360/turns)
+                ash.forward(length*spacing_multiplier**2)
+                ash.right(360/turns)
+            ash.forward(length*spacing_multiplier**3)
+            ash.right(360/turns)
+        ash.forward(length*spacing_multiplier**4)
+        ash.right(360/turns)
+    window.exitonclick()
+
+   
+def main():
+    draw_fractal()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
